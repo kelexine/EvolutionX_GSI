9c9ad9d9893697d2f1e3cd2f5d820418bee46a63
Franklin Kelechi <70432347+kelexine@users.noreply.github.com>
2023-12-08 18:09:06 +0100

    Update evo.mk

--- evo.mk
+++ evo.mk
@@ -7,7 +7,7 @@ $(call inherit-product, device/evolution/sepolicy/common/sepolicy.mk)
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
 SELINUX_IGNORE_NEVERALLOWS := true
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 720
 
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR := true
@@ -15,4 +15,4 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_TOUCHGESTURES := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/KoysX/treble_build_evo/evo/ota.json
+    ro.system.ota.json_url=https://raw.githubusercontent.com/kelexine/EvolutionX_GSI/evo/ota.json
